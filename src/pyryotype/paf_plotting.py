beb7710052abb247804cd8123d3071f3b2ee32dc
Adoni5 <roryjmunro1@gmail.com>
2024-01-10 17:02:50 +0000

    move sorting to before collapsing multiple mappings Fix bug where same mapping start breaks choosing alignment end when collapsing supplementary mappings Collapse multiple mappings to choose best alignment for contig in metadata

--- src/pyryotype/paf_plotting.py
+++ src/pyryotype/paf_plotting.py
@@ -390,6 +390,9 @@ def _collapse_multiple_mappings(alignments: Iterator[PAFProtocol]) -> Iterator[P
                     contig_mapping_proportion[(query_name, contig)], key=lambda x: x.target_start
                 )
                 paf = PAF.from_protocol(largest_proportion_alignments[0])
+                largest_proportion_alignments = sorted(
+                    contig_mapping_proportion[(query_name, contig)], key=lambda x: x.target_end
+                )
                 paf.target_end = largest_proportion_alignments[-1].target_end
                 yield paf
             curr_id = alignment.query_name
@@ -402,6 +405,9 @@ def _collapse_multiple_mappings(alignments: Iterator[PAFProtocol]) -> Iterator[P
             contig_mapping_proportion[(query_name, contig)], key=lambda x: x.target_start
         )
         paf = PAF.from_protocol(largest_proportion_alignments[0])
+        largest_proportion_alignments = sorted(
+            contig_mapping_proportion[(query_name, contig)], key=lambda x: x.target_end
+        )
         paf.target_end = largest_proportion_alignments[-1].target_end
         yield paf
 
@@ -611,10 +617,10 @@ def plot_paf_alignments(
         lambda x: x.target_name == target and x.mapping_quality >= mapq_filter,
         alignments,
     )
-    if strict == PlotMode.STRICT:
-        iterable = _collapse_multiple_mappings(iterable)
     if kwargs.get("sorted", False):
         iterable = sorted(iterable, key=lambda x: x.target_start)
+    if strict == PlotMode.STRICT:
+        iterable = _collapse_multiple_mappings(iterable)
     if filter_down == PlotMode.FILTER_DOWN:
         iterable = filter_extraneous_mappings(iterable)
 
--- src/pyryotype/paf_tools.py
+++ src/pyryotype/paf_tools.py
@@ -3,21 +3,23 @@ from collections.abc import Iterator
 from itertools import chain
 from pathlib import Path
 
-from pyryotype.paf_plotting import PAFProtocol
+from pyryotype.paf_plotting import PAFProtocol, _collapse_multiple_mappings
 
 
-def get_metadata_csv(alignments: Iterator[PAFProtocol], filename: str | Path) -> None:
+def get_metadata_csv(alignments: Iterator[PAFProtocol], filename: str | Path, mapq_threshold: int = 0) -> None:
     """
     Write out a CSV for use with bandage, which parses PAF files and
     puts out metadata for labelling various contigs in bandage, such as
     which target on the reference the contig aligns to
 
     :param alignments: An iterator which yields objects that implement the PAFProtocol methods
-    :param filename: The filename to write the metadata to. Will
+    :param filename: The filename to write the metadata to. Raises FileExistsError if the file already exists.
+    :param mapq_threshold: Filter out alignments under this threshold. Defaults 0.
     """
     headers = ["query_name", "target_name", "target_start", "target_end", "mapping_quality"]
     with open(filename, "x", newline="") as fh:
         writer = csv.writer(fh)
         writer.writerow([*headers, "blast_identity"])
-        for alignment in alignments:
-            writer.writerow(chain((getattr(alignment, h, "*") for h in headers), (alignment.blast_identity(),)))
+        for alignment in _collapse_multiple_mappings(alignments):
+            if not alignment.mapping_quality < mapq_threshold:
+                writer.writerow(chain((getattr(alignment, h, "*") for h in headers), (alignment.blast_identity(),)))
