f55b89f9c1c72af9360f53b9bef39849a862bb52
Adoni5 <roryjmunro1@gmail.com>
2024-01-10 15:15:15 +0000

    Paf utils for getting bandage metadata

--- src/pyryotype/__init__.py
+++ src/pyryotype/__init__.py
@@ -4,5 +4,14 @@
 from pyryotype.coverage import plot_coverage
 from pyryotype.ideogram import GENOME, plot_ideogram
 from pyryotype.paf_plotting import PAFProtocol, PlotMode, plot_paf_alignments
+from pyryotype.paf_tools import get_metadata_csv
 
-__all__ = ['GENOME', 'plot_ideogram', 'plot_coverage', 'plot_paf_alignments', 'PAFProtocol', 'PlotMode']
+__all__ = [
+    'GENOME',
+    'plot_ideogram',
+    'plot_coverage',
+    'plot_paf_alignments',
+    'PAFProtocol',
+    'PlotMode',
+    'get_metadata_csv',
+]
