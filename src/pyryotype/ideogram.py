27d06c2ef755a3e7afeebc189a4275bc9162e3f9
Adoni5 <roryjmunro1@gmail.com>
2024-01-16 11:52:54 +0000

    User adjustable alpha kwarg

--- src/pyryotype/ideogram.py
+++ src/pyryotype/ideogram.py
@@ -253,7 +253,7 @@ def plot_ideogram(
                 color=r_colour,
                 joinstyle="round",
                 zorder=3,
-                alpha=0.5,
+                alpha=kwargs.get("alpha", 0.5),
             )
             ax.add_patch(r)
 
